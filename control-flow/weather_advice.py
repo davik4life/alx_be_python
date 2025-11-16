1228afff8889096ab69892edb054eaabc044a891
Victor <davik4life@gmail.com>
2025-11-16 15:37:20 +0100

    Added weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,19 +1,3 @@
-# weather = ["sunny", "rainy", "cold"]
-
-# for ask in weather:
-#     if askWeather == weather[0]:
-#         print("Wear a t-shirt and sunglasses.")
-#         break
-#     elif askWeather == weather[1]:
-#         print("Don't forget your umbrella and a raincoat.")
-#         break
-#     elif askWeather == weather[2]:
-#         print("Make sure to wear a warm coat and a scarf.")
-#         break
-#     else: print("Sorry, I don't have recommendations for this weather.")
-#     break
-
-
 askWeather = str(input("What's the weather like today? (sunny/rainy/cold):")).lower()
 
 if "sunny" == askWeather:
