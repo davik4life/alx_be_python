d72c4300c9155c4a7569c3760892830302ddf255
Victor <davik4life@gmail.com>
2025-11-30 18:40:16 +0100

    Adjustments

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,11 +5,46 @@ Implement a Library class with a private list _books to store instances of Book.
 Include methods to add_book, check_out_book(title), return_book(title), and list_available_books.
 """
 
+# class Book:
+#     def __init__(self, title, author):
+#         self.title = title
+#         self.author = author
+#         self._is_checked_out = False
+
+# class Library:
+#     def __init__(self):
+#         self._books = []
+
+#     def add_book(self, book):
+#         self._books.append(book)
+
+#     def check_out_book(self, title):
+#         for book in self._books:
+#             if book.title == title and not book._is_checked_out:
+#                 book._is_checked_out = True
+#                 return True
+#         return False
+
+#     def return_book(self, title):
+#         for book in self._books:
+#             if book.title == title and book._is_checked_out:
+#                 book._is_checked_out = False
+#                 return True
+#         return False
+
+#     def list_available_books(self):
+#         available_books = [book for book in self._books if not book._is_checked_out]
+#         for book in available_books:
+#             print(f'{book.title} by {book.author}')
+#         return available_books
+
+    
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
         self._is_checked_out = False
+
 
 class Library:
     def __init__(self):
@@ -33,9 +68,4 @@ class Library:
         return False
 
     def list_available_books(self):
-        available_books = [book for book in self._books if not book._is_checked_out]
-        for book in available_books:
-            print(f'{book.title} by {book.author}')
-        return available_books
-
-    
+        return [book for book in self._books if not book._is_checked_out]
