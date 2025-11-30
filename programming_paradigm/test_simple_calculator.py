41a6a7e559807dea217615d1c7d6884758a4bcc6
Victor <davik4life@gmail.com>
2025-11-30 17:33:27 +0100

    Adjusted some phrases

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,28 +6,27 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calculator = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calculator.add(2, 6), 8)
         self.assertEqual(self.calculator.add(0, 0), 0)
         self.assertEqual(self.calculator.add(-1, 1), 0)
         self.assertEqual(self.calculator.add(-5, -3), -8)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calculator.subtract(10, 5), 5)
         self.assertEqual(self.calculator.subtract(0, 0), 0)
         self.assertEqual(self.calculator.subtract(-1, -1), 0)
         self.assertEqual(self.calculator.subtract(-5, -3), -2)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calculator.multiply(4, 2), 8)
         self.assertEqual(self.calculator.multiply(0, 5), 0)
         self.assertEqual(self.calculator.multiply(-1, 1), -1)
         self.assertEqual(self.calculator.multiply(-3, -3), 9)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calculator.divide(8, 2), 4)
         self.assertEqual(self.calculator.divide(-6, 2), -3)
         self.assertEqual(self.calculator.divide(-9, -3), 3)
         self.assertIsNone(self.calculator.divide(5, 0))
 
-        
